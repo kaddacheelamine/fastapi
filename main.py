f7f1dfcc08e9f6a7efaf63674690afb5f6c6b7f0
kaddache mohammed el amine <kaddache371@gmail.com>
2025-01-29 15:04:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List, Dict
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORS configuration
+origins = [
+    "*",  # البورت الذي يعمل عليه رياكت
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # السماح لجميع الطرق، بما في ذلك OPTIONS
+    allow_headers=["*"],  # السماح لجميع الرؤوس
+)
+
 
 class Medicine(BaseModel):
     name: str
@@ -10,13 +25,15 @@ class Medicine(BaseModel):
     frequency: str
     note: str
 
+
 class PrescriptionData(BaseModel):
     patientName: str
-    patientAge: str
+    patientAge: int
     patientDescription: str
     currentDate: str
     medicines: List[Medicine]
-    
+
+
 @app.post("/store")
 async def store_prescription(data: PrescriptionData):
     """
@@ -28,3 +45,8 @@ async def store_prescription(data: PrescriptionData):
         return {"message": "Prescription data received and printed successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error processing request: {str(e)}")
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
